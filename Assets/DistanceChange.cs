765e45b06f7bfe7729eef8742b08050a28dcdf53
Soranna350 <soranna.bacanu20@imperial.ac.uk>
2022-12-30 18:18:46 +0100

    Add SettingsMenu_draft_1

---
+++ Assets/DistanceChange.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// determine the distance
+public class DistanceChange : MonoBehaviour
+{
+    public GameObject HoloLens;
+    public GameObject Hologram;
+
+    public float Distance_;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Distance_ = Vector3.Distance(HoloLens.transform.position, Hologram.transform.position);
+    }
+}

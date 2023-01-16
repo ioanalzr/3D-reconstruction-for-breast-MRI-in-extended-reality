c8a88b62f74d839b3fbda07fbb2f7b8848f1e89e
ioanalzr <ioana.lazar20@imperial.ac.uk>
2023-01-16 04:56:43 +0200

    saving and loading model

--- Assets/Scripts/GUI/Components/RuntimeGUI.cs
+++ Assets/Scripts/GUI/Components/RuntimeGUI.cs
@@ -7,9 +7,24 @@ using System.Linq;
 //using UnityEditor.U2D.Animation;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
 
 namespace UnityVolumeRendering
 {
+
+
     /// <summary>
     /// This is a basic runtime GUI, that can be used during play mode.
     /// You can import datasets, and edit them.
@@ -17,6 +32,18 @@ namespace UnityVolumeRendering
     /// </summary>
     public class RuntimeGUI : MonoBehaviour
     {
+        public string savedPath;
+
+        //private void Start()
+
+        /*GameObject[] objects =
+        UnityEngine.Object.FindObjectsOfType<GameObject>(); // object that you want to save (get object with 3D model Tag
+        if (objects[0].tag == "3Dmodel" && objects[0].activeInHierarchy)
+        {
+            prefab = objects[0];
+        }
+    }*/
+
         private void OnGUI()
         {
             GUILayout.BeginVertical();
@@ -25,14 +52,19 @@ namespace UnityVolumeRendering
             // Show dataset import buttons
 
 
-            if (GUILayout.Button("Import DICOM dataset"))
+            if (GUILayout.Button("Start New Model"))
             {
                 RuntimeFileBrowser.ShowOpenDirectoryDialog(OnOpenDICOMDatasetResult);
+            }
+            if (GUILayout.Button("Load existing Model"))
+            {
+                RuntimeFileBrowser.ShowOpenDirectoryDialog(LoadOnOpenDICOMDatasetResult);
             }
 
 
             if (GUILayout.Button("Back to Main Menu"))
             {
+                DespawnAllDatasets();
                 LoadScene("MainMenu");
             }
 
@@ -52,55 +84,136 @@ namespace UnityVolumeRendering
             {
                 DistanceMeasureTool.ShowWindow();
             }
+            if (GUILayout.Button("Save Model"))
+                Save();
 
             GUILayout.EndVertical();
         }
-        
 
-    public void LoadScene(string SceneToLoad)
-        {
-            SceneManager.LoadScene(SceneToLoad);
-        }
 
-       private void OnOpenDICOMDatasetResult(RuntimeFileBrowser.DialogResult result)
-        {
-            if (!result.cancelled)
+
+
+            public void Save()
             {
-                // We'll only allow one dataset at a time in the runtime GUI (for simplicity)
-                DespawnAllDatasets();
 
-                bool recursive = true;
 
-                // Read all files
-                IEnumerable<string> fileCandidates = Directory.EnumerateFiles(result.path, "*.*", recursive ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly)
-                    .Where(p => p.EndsWith(".dcm", StringComparison.InvariantCultureIgnoreCase) || p.EndsWith(".dicom", StringComparison.InvariantCultureIgnoreCase) || p.EndsWith(".dicm", StringComparison.InvariantCultureIgnoreCase));
-
-                // Import the dataset
-                IImageSequenceImporter importer = ImporterFactory.CreateImageSequenceImporter(ImageSequenceFormat.DICOM);
-                IEnumerable<IImageSequenceSeries> seriesList = importer.LoadSeries(fileCandidates);
-                float numVolumesCreated = 0;
-                foreach (IImageSequenceSeries series in seriesList)
+                GameObject objectToSave = new GameObject();
+                GameObject[] objects =
+                UnityEngine.Object.FindObjectsOfType<GameObject>(); // object that you want to save (get object with 3D model Tag
+                foreach (GameObject go in objects)
                 {
-                    VolumeDataset dataset = importer.ImportSeries(series);
-                    // Spawn the object
-                    if (dataset != null)
+                    if (go.tag == "3Dmodel" && go.activeInHierarchy)
                     {
-                        VolumeRenderedObject obj = VolumeObjectFactory.CreateObject(dataset);
-                        obj.transform.position = new Vector3(numVolumesCreated, 0, 0);
-                        numVolumesCreated++;
+                        objectToSave = go;
+                        break;
+                    }
+
+                }
+
+
+                savedData data = new savedData();
+                data.position = objectToSave.transform.localPosition;
+                data.rotation = objectToSave.transform.localRotation;
+                data.scale = objectToSave.transform.localScale;
+                
+                IFormatter formatter = new BinaryFormatter();
+               //Path.Combine(Application.persistentDataPathPath.GetFileName)
+                string finalPath = Application.persistentDataPath + @"\" + Path.GetFileName(savedPath) + ".txt";
+                //using (FileStream fs = File.Create(finalPath))
+                //  {
+                //formatter.Serialize(fs, data);
+                //} 
+             string json = JsonUtility.ToJson(data);
+             File.WriteAllText(finalPath, json);
+            Debug.Log(finalPath);
+              }
+
+
+
+
+
+
+
+            public void LoadScene(string SceneToLoad)
+            {
+                SceneManager.LoadScene(SceneToLoad);
+            }
+
+            private void OnOpenDICOMDatasetResult(RuntimeFileBrowser.DialogResult result)
+            {
+                if (!result.cancelled)
+                {
+                    savedPath = result.path;
+                    // We'll only allow one dataset at a time in the runtime GUI (for simplicity)
+                    DespawnAllDatasets();
+
+                    bool recursive = true;
+
+                    // Read all files
+                    IEnumerable<string> fileCandidates = Directory.EnumerateFiles(result.path, "*.*", recursive ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly)
+                        .Where(p => p.EndsWith(".dcm", StringComparison.InvariantCultureIgnoreCase) || p.EndsWith(".dicom", StringComparison.InvariantCultureIgnoreCase) || p.EndsWith(".dicm", StringComparison.InvariantCultureIgnoreCase));
+
+                    // Import the dataset
+                    IImageSequenceImporter importer = ImporterFactory.CreateImageSequenceImporter(ImageSequenceFormat.DICOM);
+                    IEnumerable<IImageSequenceSeries> seriesList = importer.LoadSeries(fileCandidates);
+                    float numVolumesCreated = 0;
+                    foreach (IImageSequenceSeries series in seriesList)
+                    {
+                        VolumeDataset dataset = importer.ImportSeries(series);
+                        // Spawn the object
+                        if (dataset != null)
+                        {
+
+                            VolumeRenderedObject obj = VolumeObjectFactory.CreateObject(dataset,"new");
+
+
+                            obj.transform.position = new Vector3(numVolumesCreated, 0, 0);
+                            numVolumesCreated++;
+                        }
                     }
                 }
             }
-        }
-
-        private void DespawnAllDatasets()
-        {
-            VolumeRenderedObject[] volobjs = GameObject.FindObjectsOfType<VolumeRenderedObject>();
-            foreach (VolumeRenderedObject volobj in volobjs)
+            private void LoadOnOpenDICOMDatasetResult(RuntimeFileBrowser.DialogResult result)
             {
-                GameObject.Destroy(volobj.gameObject);
+                if (!result.cancelled)
+                {
+                    savedPath = result.path;
+                    // We'll only allow one dataset at a time in the runtime GUI (for simplicity)
+                    DespawnAllDatasets();
+
+                    bool recursive = true;
+
+                    // Read all files
+                    IEnumerable<string> fileCandidates = Directory.EnumerateFiles(result.path, "*.*", recursive ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly)
+                        .Where(p => p.EndsWith(".dcm", StringComparison.InvariantCultureIgnoreCase) || p.EndsWith(".dicom", StringComparison.InvariantCultureIgnoreCase) || p.EndsWith(".dicm", StringComparison.InvariantCultureIgnoreCase));
+
+                    // Import the dataset
+                    IImageSequenceImporter importer = ImporterFactory.CreateImageSequenceImporter(ImageSequenceFormat.DICOM);
+                    IEnumerable<IImageSequenceSeries> seriesList = importer.LoadSeries(fileCandidates);
+                    float numVolumesCreated = 0;
+                    foreach (IImageSequenceSeries series in seriesList)
+                    {
+                        VolumeDataset dataset = importer.ImportSeries(series);
+                        // Spawn the object
+                        if (dataset != null)
+                        {
+                            VolumeRenderedObject obj = VolumeObjectFactory.CreateObject(dataset, "old", result.path);
+                            obj.transform.position = new Vector3(numVolumesCreated, 0, 0);
+                            numVolumesCreated++;
+                        }
+                    }
+                }
             }
-        }
+
+            private void DespawnAllDatasets()
+            {
+                VolumeRenderedObject[] volobjs = GameObject.FindObjectsOfType<VolumeRenderedObject>();
+                foreach (VolumeRenderedObject volobj in volobjs)
+                {
+                    GameObject.Destroy(volobj.gameObject);
+                }
+            }
+        
     }
 }
 
--- Assets/Scripts/VolumeObject/VolumeObjectFactory.cs
+++ Assets/Scripts/VolumeObject/VolumeObjectFactory.cs
@@ -2,22 +2,109 @@
 using UnityEngine;
 using Microsoft.MixedReality.Toolkit.UI;
 using Microsoft.MixedReality.Toolkit.Input;
+using System;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace UnityVolumeRendering
 {
+    [Serializable]
+    public class savedData
+    {
+        public Vector3 position;
+        public Quaternion rotation;
+        public Vector3 scale;
+        public string notes;
+
+    }
     public class VolumeObjectFactory
     {
-        public static VolumeRenderedObject CreateObject(VolumeDataset dataset)
+        public static VolumeRenderedObject CreateObject(VolumeDataset dataset,string state="", string path="")
         {
+            
             GameObject outerObject = new GameObject("VolumeRenderedObject_" + dataset.datasetName);
+            //outerObject.tag = "3Dmodel";                                        
             VolumeRenderedObject volObj = outerObject.AddComponent<VolumeRenderedObject>();
-
+            //volObj.tag = "3Dmodel";
             GameObject meshContainer = GameObject.Instantiate((GameObject)Resources.Load("VolumeContainer"));
-            meshContainer.transform.parent = outerObject.transform;
-            meshContainer.transform.localScale = Vector3.one;
-            meshContainer.transform.localPosition = new Vector3(0,1,0);
-            meshContainer.transform.parent = outerObject.transform;
-            outerObject.transform.localRotation = Quaternion.Euler(90.0f, 0.0f, 0.0f);
+            meshContainer.tag = "3Dmodel";
+            
+            if (state == "old")
+            {
+                
+                string finalPath = Application.persistentDataPath + @"\" + Path.GetFileName(path) + ".txt";
+                savedData datacontainer = getdata(finalPath);
+                meshContainer.transform.parent = outerObject.transform;
+                meshContainer.transform.localScale = datacontainer.scale;//
+                meshContainer.transform.localPosition = datacontainer.position; //
+                meshContainer.transform.parent = outerObject.transform;
+                outerObject.transform.localRotation = datacontainer.rotation; //
+            }
+            else
+            {
+                meshContainer.transform.parent = outerObject.transform;
+                meshContainer.transform.localScale = new Vector3(0.7, 0.7, 0.7);
+                meshContainer.transform.localPosition = new Vector3(0, 1, 0);
+                meshContainer.transform.parent = outerObject.transform;
+                outerObject.transform.localRotation = Quaternion.Euler(90.0f, 0.0f, 0.0f);
+            }
+
+            MeshRenderer meshRenderer = meshContainer.GetComponent<MeshRenderer>();
+            meshRenderer.sharedMaterial = new Material(meshRenderer.sharedMaterial);
+            volObj.meshRenderer = meshRenderer;
+            volObj.dataset = dataset;
+
+            const int noiseDimX = 512;
+            const int noiseDimY = 512;
+            Texture2D noiseTexture = NoiseTextureGenerator.GenerateNoiseTexture(noiseDimX, noiseDimY);
+
+            TransferFunction tf = TransferFunctionDatabase.CreateTransferFunction();
+            Texture2D tfTexture = tf.GetTexture();
+            volObj.transferFunction = tf;
+
+            TransferFunction2D tf2D = TransferFunctionDatabase.CreateTransferFunction2D();
+            volObj.transferFunction2D = tf2D;
+
+            meshRenderer.sharedMaterial.SetTexture("_DataTex", dataset.GetDataTexture());
+            meshRenderer.sharedMaterial.SetTexture("_GradientTex", null);
+            meshRenderer.sharedMaterial.SetTexture("_NoiseTex", noiseTexture);
+            meshRenderer.sharedMaterial.SetTexture("_TFTex", tfTexture);
+
+            meshRenderer.sharedMaterial.EnableKeyword("MODE_DVR");
+            meshRenderer.sharedMaterial.DisableKeyword("MODE_MIP"); // tried to set this to enable to make it default render mode - didn't work
+            meshRenderer.sharedMaterial.DisableKeyword("MODE_SURF");
+
+            if(dataset.scaleX != 0.0f && dataset.scaleY != 0.0f && dataset.scaleZ != 0.0f)
+            {
+                float maxScale = Mathf.Max(dataset.scaleX, dataset.scaleY, dataset.scaleZ);
+                volObj.transform.localScale = new Vector3(dataset.scaleX / maxScale, dataset.scaleY / maxScale, dataset.scaleZ / maxScale);
+            }
+            meshContainer.AddComponent<ConstraintManager>();
+            meshContainer.AddComponent<ObjectManipulator>();
+            meshContainer.AddComponent<BoxCollider>();
+            meshContainer.AddComponent<NearInteractionGrabbable>();
+            
+            return volObj;
+        }
+        /*
+        public static VolumeRenderedObject LoadObject(VolumeDataset dataset, string path)
+        {
+            string finalPath = Application.persistentDataPath + @"\" + Path.GetFileName(path) + ".txt";
+            savedData datacontainer = getdata(finalPath);
+
+            GameObject outerObject = new GameObject("VolumeRenderedObject_" + dataset.datasetName);
+            //outerObject.tag = "3Dmodel";
+            VolumeRenderedObject volObj = outerObject.AddComponent<VolumeRenderedObject>();
+            GameObject meshContainer = GameObject.Instantiate((GameObject)Resources.Load("VolumeContainer"));
+            meshContainer.transform.parent = outerObject.transform; 
+            meshContainer.transform.localScale = datacontainer.scale;//
+            meshContainer.transform.localPosition = datacontainer.position; //
+           // meshContainer.transform.parent = outerObject.transform;
+            outerObject.transform.localRotation = datacontainer.rotation; //
 
             MeshRenderer meshRenderer = meshContainer.GetComponent<MeshRenderer>();
             meshRenderer.sharedMaterial = new Material(meshRenderer.sharedMaterial);
@@ -44,7 +131,7 @@ namespace UnityVolumeRendering
             meshRenderer.sharedMaterial.DisableKeyword("MODE_MIP"); // tired to set this to enable to make it default render mode - didn't work
             meshRenderer.sharedMaterial.DisableKeyword("MODE_SURF");
 
-            if(dataset.scaleX != 0.0f && dataset.scaleY != 0.0f && dataset.scaleZ != 0.0f)
+            if (dataset.scaleX != 0.0f && dataset.scaleY != 0.0f && dataset.scaleZ != 0.0f)
             {
                 float maxScale = Mathf.Max(dataset.scaleX, dataset.scaleY, dataset.scaleZ);
                 volObj.transform.localScale = new Vector3(dataset.scaleX / maxScale, dataset.scaleY / maxScale, dataset.scaleZ / maxScale);
@@ -52,8 +139,19 @@ namespace UnityVolumeRendering
             meshContainer.AddComponent<ConstraintManager>();
             meshContainer.AddComponent<ObjectManipulator>();
             meshContainer.AddComponent<NearInteractionGrabbable>();
+            meshContainer.AddComponent<BoxCollider>();
             return volObj;
         }
+        */
+        public static savedData getdata(string path)
+        {
+            string json = File.ReadAllText(path);
+            Debug.Log(json);
+            savedData data = JsonUtility.FromJson<savedData>(json);
+            return data; 
+         }
+
+
 
         public static void SpawnCrossSectionPlane(VolumeRenderedObject volobj)
         {
